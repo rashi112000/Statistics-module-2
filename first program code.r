e22ca8508c1b42e97023f0d34cdf069affce8bf1
rashi112000 <114755952+rashi112000@users.noreply.github.com>
2022-11-22 18:00:06 +0100

    Create first program code.r

---
+++ first program code.r
@@ -0,0 +1,26 @@
+print("hello world!") # this command is going to display something on our screen 
+# print here is a function- it carries out some task 
+# need ("") this for print to read the command
+# ctrl+ enter is used to run the code or that specific line, select the whole command if it is more than a line
+">" this greater than symbol is called prompt
+# the command displayed on screen is also withtin double quotes
+2 + 5 # here " + " is also a fucntion 
+# " - " for subtraction 
+# " * " for multiplication
+# " / " for division
+# " ** " or " ^ " for exponentiation
+# for integer division, the symbol is " % / % " [5/3= 1.333 so, 5 %/% = 1], it removes the decimal part that's it
+# for modulo- the symbol is " %% ", meaning the number which we were not able to divide, the remainder type [5 %% 3= 2]
+# ctrl + shift + C helps to comment multiple lines as a comment- we select them all and then use "ctrl+shift+C"
+# "ctrl + shift + Z" is to redo the command while "ctrl + Z" is to undo the command 
+# how to make a variable 
+# variable name <- "value we want to store" 
+# "<-" an operator - it is called assignment operator
+# can check the variables we created in the environment tab
+# to seperate 2 words we use underscore, full stop (.) or abcDef (seperating them with capital alphabet) , eg: my_name <- rashi [or my.name or myName]
+# if we put the designated assigned variable within parenthesis, after running the commnad, the code will also be visible apart from the results
+# "alt" + "-" key will help us put " <- " automatically
+# the funtion whose value you want to store should be placed on the right and you don't need to put double codes if you are using a function
+# help("insert the function") # why ther command is not working, we will check the documentation 
+# we can replace the "help" fucntion with "?"
+# "{}" this tells us the name of the package in which the function is present
